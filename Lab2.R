5a4e2d311d80e46c2cc01b0c10e0cc808594afd4
julimatos <matosjuliana92@gmail.com>
2016-01-18 11:17:54 -0800

    put both our names on there

--- Lab2.R
+++ Lab2.R
@@ -1,13 +1,15 @@
 ########################
-#Sarah McCutcheon
+#Sarah McCutcheon & Juliana Matos
 #ESM 296 Lab 2
 #######################
-#SurveyData<-read.table("C:/Users/sarah/Documents/296/FullD.csv", header= TRUE, na.strings = 69)
+#SurveyData<-read.table("C:/Users/sarah/Documents/296/FullD.csv", header= TRUE)
 
 #View(SurveyData)
 
-plotsurvey<-lm(years~ fireRisk,data=FullD)
+plotsurvey<-lm(years~ fireRisk,data=FullData)
 
-plot(FullD$fireRisk, FullD$years)
+counts <- table(FullData$fireRisk)
+barplot(counts, main="Fire Risk Distribution", xlab="Level of Risk", ylab="Frequency")
 
-#### <(*_*)> ####
+
+###
