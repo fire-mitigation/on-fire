85618b610b453aa93d36a890bc8ce0113bd307f9
julimatos <matosjuliana92@gmail.com>
2016-01-18 11:33:13 -0800

    bar plot for "assignment" branch

--- Lab2.R
+++ Lab2.R
@@ -11,3 +11,8 @@ plotsurvey<-lm(years~ fireRisk,data=FullData)
 
 counts <- table(FullData$fireRisk)
 barplot(counts, main="Fire Risk Distribution", xlab="Level of Risk", ylab="Frequency")
+
+#####
+# new bar plot looking at distribution of chipping services used
+counts1 <- table(FullData$chip)
+barplot(counts1, main="Uses of Chipping Services", xlab="Number of Uses", ylab="Frequency")
