239f2204dc1657fcd7cf2a47a7df5b622731fb23
smccutcheon <sarahmccutch@gmail.com>
2016-01-18 11:12:54 -0800

    New bar plot of fire risk

--- Lab2.R
+++ Lab2.R
@@ -2,10 +2,12 @@
 #Sarah McCutcheon
 #ESM 296 Lab 2
 #######################
-#SurveyData<-read.table("C:/Users/sarah/Documents/296/FullD.csv", header= TRUE, na.strings = 69)
+#SurveyData<-read.table("C:/Users/sarah/Documents/296/FullData.csv", header= TRUE)
 
 #View(SurveyData)
 
-plotsurvey<-lm(years~ fireRisk,data=FullD)
+plotsurvey<-lm(years~ fireRisk,data=FullData)
 
-plot(FullD$fireRisk, FullD$years)
+counts<-table(FullData$fireRisk)
+
+barplot(counts, main="Fire Risk Distribution", xlab="Level of Risk", ylab="frequency")
